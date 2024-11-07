7f99af1f9125bc2241377ae09967b4a73b0018cf
AmanuelNBekele <amanuelnbekele@gmail.com>
2024-11-06 22:30:25 -0600

    remove resources from ui.py

--- ui.py
+++ ui.py
@@ -167,29 +167,3 @@ class SOS:
             winner = "It's a tie!"
 
         messagebox.showinfo("Game Over", winner)
-                                                                                                                       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # RESOURCES USED LISTED BELOW
-    # Youtube: https://www.youtube.com/watch?v=YXPyB4XeYLA
-    # Website: https://www.geeksforgeeks.org/tic-tac-toe-game-with-gui-using-tkinter-in-python/
-    # Youtube: https://www.youtube.com/watch?v=xx0qmpuA-vM
-    # Youtube: https://www.youtube.com/watch?v=IJ-iVnN09-8
-    # Website: https://www.geeksforgeeks.org/python-tkinter-entry-widget/
