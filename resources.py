a119b073b1fb8ff2accd8eff1b75522e980221f1
AmanuelNBekele <amanuelnbekele@gmail.com>
2024-11-06 22:29:26 -0600

    file for resources used

---
+++ resources.py
@@ -0,0 +1,6 @@
+    # RESOURCES USED LISTED BELOW
+    # Youtube: https://www.youtube.com/watch?v=YXPyB4XeYLA
+    # Website: https://www.geeksforgeeks.org/tic-tac-toe-game-with-gui-using-tkinter-in-python/
+    # Youtube: https://www.youtube.com/watch?v=xx0qmpuA-vM
+    # Youtube: https://www.youtube.com/watch?v=IJ-iVnN09-8
+    # Website: https://www.geeksforgeeks.org/python-tkinter-entry-widget/
