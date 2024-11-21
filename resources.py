4a375ab4f5d10261eeeff00c70652d88763fe41e
AmanuelNBekele <amanuelnbekele@gmail.com>
2024-11-20 21:47:50 -0600

    Sprint 4 Resources

--- resources.py
+++ resources.py
@@ -4,3 +4,4 @@
     # Youtube: https://www.youtube.com/watch?v=xx0qmpuA-vM
     # Youtube: https://www.youtube.com/watch?v=IJ-iVnN09-8
     # Website: https://www.geeksforgeeks.org/python-tkinter-entry-widget/
+    # Youtube: https://www.youtube.com/watch?v=6ZmgIBraKgU
